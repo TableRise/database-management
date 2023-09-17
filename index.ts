1cda2daae23739ccd85072268d2683149b827ef8
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-09-17 18:04:15 -0300

    ref: Remove export of schemas

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
-import { z } from 'zod';
 import mongoose from 'mongoose';
 
 import models from './src/models';
 import MongoModel from './src/models/MongoModel';
 import schemas, { SchemasDnDType, SchemasUserType } from './src/schemas';
-import { SystemContent } from './src/schemas/dungeons&dragons5e/systemValidationSchema';
 import { DnDEntities, ModelEntity, UserEntities } from './src/types/DatabaseEntities';
 
 const path = require('path');
@@ -38,52 +36,4 @@ export default class DatabaseManagement {
     }
 }
 
-export { MongoModel, SchemasDnDType, SchemasUserType, SystemContent, mongoose };
-
-export interface Internacional<T> {
-    _id?: string;
-    active?: boolean;
-    en: T;
-    pt: T;
-}
-
-const updateContent = schemas['dungeons&dragons5e'].updateContentZodSchema;
-export type UpdateContent = z.infer<typeof updateContent>
-
-// Dungeons & Dragons Schemas Type Export
-const dndarmor = schemas['dungeons&dragons5e'].armorZod;
-const dndbackground = schemas['dungeons&dragons5e'].backgroundZod;
-const dndclasses = schemas['dungeons&dragons5e'].classZod;
-const dndfeat = schemas['dungeons&dragons5e'].featZod;
-const dndgod = schemas['dungeons&dragons5e'].godZod;
-const dnditem = schemas['dungeons&dragons5e'].itemZod;
-const dndmagicItem = schemas['dungeons&dragons5e'].magicItemZod;
-const dndmonster = schemas['dungeons&dragons5e'].monsterZod;
-const dndrace = schemas['dungeons&dragons5e'].raceZod;
-const dndrealm = schemas['dungeons&dragons5e'].realmZod;
-const dndspell = schemas['dungeons&dragons5e'].spellZod;
-const dndsystem = schemas['dungeons&dragons5e'].systemZod.systemZod;
-const dndsystempayload = schemas['dungeons&dragons5e'].systemZod.systemPayloadZodSchema;
-const dndweapon = schemas['dungeons&dragons5e'].weaponZod;
-const dndwiki = schemas['dungeons&dragons5e'].wikiZod;
-export type DnDArmor = z.infer<typeof dndarmor>
-export type DnDBackground = z.infer<typeof dndbackground>
-export type DnDClass = z.infer<typeof dndclasses>
-export type DnDFeat = z.infer<typeof dndfeat>
-export type DnDGod = z.infer<typeof dndgod>
-export type DnDItem = z.infer<typeof dnditem>
-export type DnDMagicItem = z.infer<typeof dndmagicItem>
-export type DnDMonster = z.infer<typeof dndmonster>
-export type DnDRace = z.infer<typeof dndrace>
-export type DnDRealm = z.infer<typeof dndrealm>
-export type DnDSpell = z.infer<typeof dndspell>
-export type DnDSystem = z.infer<typeof dndsystem>
-export type DnDSystemPayload = z.infer<typeof dndsystempayload>
-export type DnDWeapon = z.infer<typeof dndweapon>
-export type DnDWiki = z.infer<typeof dndwiki>
-
-// User Schemas Type Export
-const user = schemas.user.userZod;
-const userDetails = schemas.user.userDetailZod;
-export type User = z.infer<typeof user>
-export type UserDetail = z.infer<typeof userDetails>
+export { MongoModel, mongoose };
