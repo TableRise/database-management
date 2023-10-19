bc1ad5e342770fc765f0fd3257b34be56764b128
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-10-18 22:44:17 -0300

    ref: Update

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -4,20 +4,24 @@ import ModelType from '../types/Model';
 abstract class MongoModel<T> implements ModelType<T> {
     constructor(protected _model: Model<T>) {}
 
+    private _formatDbQuery(queryObj: unknown | null): T | T[] {
+        return JSON.parse(JSON.stringify(queryObj));
+    }
+
     public async create(payload: T): Promise<T> {
-        return await this._model.create({ ...payload });
+        return this._formatDbQuery(await this._model.create({ ...payload })) as T;
     }
 
     public async findAll(query = {}): Promise<T[]> {
-        return await this._model.find(query);
+        return this._formatDbQuery(await this._model.find(query)) as T[];
     }
 
     public async findOne(_id: string): Promise<T | null> {
-        return await this._model.findOne({ _id });
+        return await this._formatDbQuery(this._model.findOne({ _id })) as T;
     }
 
     public async update(_id: string, payload: T): Promise<T | null> {
-        return await this._model.findByIdAndUpdate({ _id }, { ...payload } as UpdateQuery<T>, { new: true });
+        return await this._formatDbQuery(this._model.findByIdAndUpdate({ _id }, { ...payload } as UpdateQuery<T>, { new: true })) as T;
     }
 
     public async delete(_id: string): Promise<T | null> {
