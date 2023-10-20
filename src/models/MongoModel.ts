35be2778b28f69a9b569e7a3f0a94f991e484e67
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-10-20 08:53:40 -0300

    Revert "ref: Update"
    This reverts commit bc1ad5e342770fc765f0fd3257b34be56764b128.

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -4,24 +4,20 @@ import ModelType from '../types/Model';
 abstract class MongoModel<T> implements ModelType<T> {
     constructor(protected _model: Model<T>) {}
 
-    private _formatDbQuery(queryObj: unknown | null): T | T[] {
-        return JSON.parse(JSON.stringify(queryObj));
-    }
-
     public async create(payload: T): Promise<T> {
-        return this._formatDbQuery(await this._model.create({ ...payload })) as T;
+        return await this._model.create({ ...payload });
     }
 
     public async findAll(query = {}): Promise<T[]> {
-        return this._formatDbQuery(await this._model.find(query)) as T[];
+        return await this._model.find(query);
     }
 
     public async findOne(_id: string): Promise<T | null> {
-        return await this._formatDbQuery(this._model.findOne({ _id })) as T;
+        return await this._model.findOne({ _id });
     }
 
     public async update(_id: string, payload: T): Promise<T | null> {
-        return await this._formatDbQuery(this._model.findByIdAndUpdate({ _id }, { ...payload } as UpdateQuery<T>, { new: true })) as T;
+        return await this._model.findByIdAndUpdate({ _id }, { ...payload } as UpdateQuery<T>, { new: true });
     }
 
     public async delete(_id: string): Promise<T | null> {
