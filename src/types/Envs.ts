6c817f7217081f59d2020bd9e9bbda9acf101558
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-12-24 00:53:40 -0300

    ref: Rollback

--- src/types/Envs.ts
+++ src/types/Envs.ts
@@ -1,13 +1,7 @@
-export interface MongooseEnvs {
+export interface Envs {
     db_username: string;
     db_password: string;
     db_host: string;
     db_database: string;
     db_initialString: string;
-}
-
-export interface RedisEnvs {
-    redis_host: string;
-    redis_port: string;
-    redis_password: string;
 }
