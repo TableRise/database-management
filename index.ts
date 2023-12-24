51a9f82c4deb7af59dd4316f6d363c91f73fa0fb
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-12-24 01:03:06 -0300

    ref: Update

--- index.ts
+++ index.ts
@@ -1,27 +1,47 @@
 import mongoose from 'mongoose';
-
+import * as redis from 'redis';
 import models from './src/models';
 import MongoModel from './src/models/MongoModel';
 import { DnDEntities, ModelEntity, UserEntities } from './src/types/DatabaseEntities';
-import { Envs } from './src/types/Envs';
+import { MongooseEnvs, RedisEnvs } from './src/types/Envs';
 
 const path = require('path');
 
 export default class DatabaseManagement {
-    static connect(on: boolean = false, altEnvs: Envs = {} as Envs): Promise<mongoose.Mongoose> {
-        if (on) {
-            const tableriseEnvs = require(path.resolve('./tablerise.environment.js'));
+    static mongoose(altEnvs: MongooseEnvs = {} as MongooseEnvs) {
+        const tableriseEnvs = require(path.resolve('./tablerise.environment.js'));
 
-            const username = altEnvs.db_username || tableriseEnvs.database_username;
-            const password = altEnvs.db_password || tableriseEnvs.database_password;
-            const host = altEnvs.db_host || tableriseEnvs.database_host;
-            const database = altEnvs.db_database || tableriseEnvs.database_database;
-            const initialString = altEnvs.db_initialString || tableriseEnvs.database_initialString;
+        const username = altEnvs.db_username || tableriseEnvs.database_username;
+        const password = altEnvs.db_password || tableriseEnvs.database_password;
+        const host = altEnvs.db_host || tableriseEnvs.database_host;
+        const database = altEnvs.db_database || tableriseEnvs.database_database;
+        const initialString = altEnvs.db_initialString || tableriseEnvs.database_initialString;
 
-            return mongoose.connect(`${initialString}://${username}:${password}@${host}/${database}`);
-        }
+        return mongoose.connect(`${initialString}://${username}:${password}@${host}/${database}`);
+    }
 
-        return;
+    static redis(altEnvs: RedisEnvs = {} as RedisEnvs) {
+        const tableriseEnvs = require(path.resolve('./tablerise.environment.js'));
+
+        const host = altEnvs.redis_host || tableriseEnvs.redis_host;
+        const port = altEnvs.redis_port || tableriseEnvs.redis_port;
+        const password = altEnvs.redis_password || tableriseEnvs.redis_password;
+
+        const client = redis.createClient({
+            password,
+            socket: {
+                host,
+                port
+            }
+        })
+
+        client.connect()
+
+        return client;
+    }
+
+    static connect(on: boolean = false, db: 'mongoose' | 'redis', altEnvs: MongooseEnvs & RedisEnvs = {} as MongooseEnvs & RedisEnvs): Promise<any> | any {
+        if (on) return DatabaseManagement[db](altEnvs);
     }
 
     public modelInstance(modelEntity: ModelEntity, entity: DnDEntities | UserEntities): MongoModel<any> {
--- src/types/Envs.ts
+++ src/types/Envs.ts
@@ -1,7 +1,13 @@
-export interface Envs {
+export interface MongooseEnvs {
     db_username: string;
     db_password: string;
     db_host: string;
     db_database: string;
     db_initialString: string;
+}
+
+export interface RedisEnvs {
+    redis_host: string;
+    redis_port: string;
+    redis_password: string;
 }
