c179c8bd1333927d45b98d28afab2ff19f5d7a84
Rafael Santos <rafaelcs_94@hotmail.com>
2024-02-25 14:10:23 -0300

    feat: update modelInstance method with campaigns

--- src/types/DatabaseEntities.ts
+++ src/types/DatabaseEntities.ts
@@ -1,5 +1,21 @@
-export type ModelEntity = 'dungeons&dragons5e' | 'user';
+export type ModelEntity = 'dungeons&dragons5e' | 'user' | 'campaign';
 
-export type DnDEntities = 'Armors' | 'Backgrounds' | 'Classes' | 'Feats' | 'Gods' | 'Items' | 'MagicItems' | 'Monsters' | 'Races' | 'Realms' | 'Spells' | 'System' | 'Weapons' | 'Wikis';
+export type DnDEntities =
+  | 'Armors'
+  | 'Backgrounds'
+  | 'Classes'
+  | 'Feats'
+  | 'Gods'
+  | 'Items'
+  | 'MagicItems'
+  | 'Monsters'
+  | 'Races'
+  | 'Realms'
+  | 'Spells'
+  | 'System'
+  | 'Weapons'
+  | 'Wikis';
 
 export type UserEntities = 'Users' | 'UserDetails';
+
+export type CampaignsEntities = 'Campaigns';
