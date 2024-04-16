cc61fd007d93e2b1075ff2084499bf45253e9fca
Rafael Santos <rafaelcs_94@hotmail.com>
2024-04-16 11:13:37 -0300

    feat: update Player schema and interface

--- src/interfaces/Campaigns.ts
+++ src/interfaces/Campaigns.ts
@@ -1,106 +1,107 @@
 export interface ImageObject {
-  id: string;
-  link: string;
-  uploadDate: string;
+  id: string
+  link: string
+  uploadDate: string
 }
 
 export interface MainStory {
-  title: string;
-  lore: string;
-  image: ImageObject;
-  createdAt: string;
-  updatedAt: string;
+  title: string
+  lore: string
+  image: ImageObject
+  createdAt: string
+  updatedAt: string
 }
 
 export interface Environment {
-  title: string;
-  lore: string;
-  environmentImage: ImageObject;
-  createdAt: string;
-  updatedAt: string;
+  title: string
+  lore: string
+  environmentImage: ImageObject
+  createdAt: string
+  updatedAt: string
 }
 
 export interface CharacterLore {
-  characterId: string;
-  lore: string;
-  createdAt: string;
-  updatedAt: string;
+  characterId: string
+  lore: string
+  createdAt: string
+  updatedAt: string
 }
 
 export interface Lores {
-  playerCharacters: CharacterLore[];
-  dungeonMasterCharacters: CharacterLore[];
-  environments: Environment[];
-  mainHistory: MainStory[];
+  playerCharacters: CharacterLore[]
+  dungeonMasterCharacters: CharacterLore[]
+  environments: Environment[]
+  mainHistory: MainStory[]
 }
 
 export interface Announcement {
-  title: string;
-  author: string;
-  content: string;
+  title: string
+  author: string
+  content: string
 }
 
 export interface Infos {
-  campaignAge: string;
-  matchDates: string[];
-  announcements: Announcement[];
-  visibility: 'hidden' | 'visible';
+  campaignAge: string
+  matchDates: string[]
+  announcements: Announcement[]
+  visibility: 'hidden' | 'visible'
 }
 
 export interface Log {
-  loggedAt: string;
-  content: string;
+  loggedAt: string
+  content: string
 }
 
 export interface Music {
-  title: string;
-  youtubeLink: string;
+  title: string
+  youtubeLink: string
 }
 
 export interface Size {
-  width: number;
-  height: number;
+  width: number
+  height: number
 }
 
 export interface Position {
-  x: number;
-  y: number;
+  x: number
+  y: number
 }
 
 export interface Avatar {
-  avatarId: string;
-  userId: string;
-  picture: ImageObject;
-  position: Position;
-  size: Size;
-  status: 'alive' | 'dead' | 'viewer';
+  avatarId: string
+  userId: string
+  picture: ImageObject
+  position: Position
+  size: Size
+  status: 'alive' | 'dead' | 'viewer'
 }
 
 export interface MatchData {
-  matchId: string;
-  avatars: Avatar[];
-  musics: Music[];
-  mapImages: ImageObject[];
-  logs: Log[];
+  matchId: string
+  avatars: Avatar[]
+  musics: Music[]
+  mapImages: ImageObject[]
+  logs: Log[]
 }
 
 export interface Player {
-  userId: string;
-  characterIds: string[];
-  role: 'admin_player' | 'dungeon_master' | 'player';
+  userId: string
+  characterIds: string[]
+  role: 'admin_player' | 'dungeon_master' | 'player'
+  status: 'pending' | 'active' | 'inactive' | 'banned'
 }
 
 export default interface Campaign {
-  campaignId?: string;
-  title: string;
-  cover: ImageObject;
-  ageRestriction: number;
-  system: 'd&d5e' | 'other';
-  description: string;
-  campaignPlayers: Player[];
-  matchData: MatchData;
-  infos: Infos;
-  lores: Lores;
-  createdAt: string;
-  updatedAt: string;
+  campaignId?: string
+  title: string
+  cover: ImageObject
+  ageRestriction: number
+  system: 'd&d5e' | 'other'
+  description: string
+  campaignPlayers: Player[]
+  matchData: MatchData
+  infos: Infos
+  lores: Lores
+  createdAt: string
+  updatedAt: string
 }
--- src/models/campaigns/CampaignsModel.ts
+++ src/models/campaigns/CampaignsModel.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose'
 import Campaign, {
   Announcement,
   Avatar,
@@ -13,19 +13,19 @@ import Campaign, {
   Music,
   Player,
   Position,
-  Size,
-} from '../../interfaces/Campaigns';
-import newUUID from '../../helpers/newUUID';
-import MongoModel from '../MongoModel';
+  Size
+} from '../../interfaces/Campaigns'
+import newUUID from '../../helpers/newUUID'
+import MongoModel from '../MongoModel'
 
 const imageObjectMongooseSchema = new Schema<ImageObject>(
   {
     id: { type: String },
     link: { type: String },
-    uploadDate: { type: String },
+    uploadDate: { type: String }
   },
   { _id: false }
-);
+)
 
 const mainStoryMongooseSchema = new Schema<MainStory>(
   {
@@ -33,10 +33,10 @@ const mainStoryMongooseSchema = new Schema<MainStory>(
     lore: { type: String, required: true },
     image: { type: imageObjectMongooseSchema, required: true },
     createdAt: { type: String, required: true },
-    updatedAt: { type: String, required: true },
+    updatedAt: { type: String, required: true }
   },
   { _id: false }
-);
+)
 
 const environmentMongooseSchema = new Schema<Environment>(
   {
@@ -44,84 +44,84 @@ const environmentMongooseSchema = new Schema<Environment>(
     lore: { type: String, required: true },
     environmentImage: { type: imageObjectMongooseSchema, required: true },
     createdAt: { type: String, required: true },
-    updatedAt: { type: String, required: true },
+    updatedAt: { type: String, required: true }
   },
   { _id: false }
-);
+)
 
 const characterMongooseSchema = new Schema<CharacterLore>(
   {
     characterId: { type: String, required: true },
     lore: { type: String, required: true },
     createdAt: { type: String, required: true },
-    updatedAt: { type: String, required: true },
+    updatedAt: { type: String, required: true }
   },
   { _id: false }
-);
+)
 
 const loresMongooseSchema = new Schema<Lores>(
   {
     playerCharacters: { type: [characterMongooseSchema], required: true },
     dungeonMasterCharacters: {
       type: [characterMongooseSchema],
-      required: true,
+      required: true
     },
     environments: { type: [environmentMongooseSchema], required: true },
-    mainHistory: { type: [mainStoryMongooseSchema], required: true },
+    mainHistory: { type: [mainStoryMongooseSchema], required: true }
   },
   { _id: false }
-);
+)
 
 const announcementMongooseSchema = new Schema<Announcement>(
   {
     title: { type: String, required: true },
     author: { type: String, required: true },
-    content: { type: String, required: true },
+    content: { type: String, required: true }
   },
   { _id: false }
-);
+)
 
 const infosMongooseSchema = new Schema<Infos>(
   {
     campaignAge: { type: String, required: true },
     matchDates: { type: [String], required: true },
     announcements: { type: [announcementMongooseSchema], required: true },
-    visibility: { type: String, required: true },
+    visibility: { type: String, required: true }
   },
   { _id: false }
-);
+)
 
 const logMongooseSchema = new Schema<Log>(
   {
     loggedAt: { type: String, required: true },
-    content: { type: String, required: true },
+    content: { type: String, required: true }
   },
   { _id: false }
-);
+)
 
 const musicMongooseSchema = new Schema<Music>(
   {
     title: { type: String, required: true },
-    youtubeLink: { type: String, required: true },
+    youtubeLink: { type: String, required: true }
   },
   { _id: false }
-);
+)
 
 const sizeMongooseSchema = new Schema<Size>(
   {
     width: { type: Number, required: true },
-    height: { type: Number, required: true },
+    height: { type: Number, required: true }
   },
   { _id: false }
-);
+)
 
 const positionMongooseSchema = new Schema<Position>(
   {
     x: { type: Number, required: true },
-    y: { type: Number, required: true },
+    y: { type: Number, required: true }
   },
   { _id: false }
-);
+)
 
 const avatarMongooseSchema = new Schema<Avatar>(
   {
@@ -130,10 +130,10 @@ const avatarMongooseSchema = new Schema<Avatar>(
     picture: { type: imageObjectMongooseSchema, required: true },
     position: { type: positionMongooseSchema, required: true },
     size: { type: sizeMongooseSchema, required: true },
-    status: { type: String, required: true },
+    status: { type: String, required: true }
   },
   { _id: false }
-);
+)
 
 const matchDataMongooseSchema = new Schema<MatchData>(
   {
@@ -141,19 +141,20 @@ const matchDataMongooseSchema = new Schema<MatchData>(
     avatars: { type: [avatarMongooseSchema], required: true },
     musics: { type: [musicMongooseSchema], required: true },
     mapImages: { type: [imageObjectMongooseSchema], required: true },
-    logs: { type: [logMongooseSchema], required: true },
+    logs: { type: [logMongooseSchema], required: true }
   },
   { _id: false }
-);
+)
 
 const playerMongooseSchema = new Schema<Player>(
   {
     userId: { type: String, required: true },
     characterIds: { type: [String], required: true },
     role: { type: String, required: true },
+    status: { type: String, required: true }
   },
   { _id: false }
-);
+)
 
 const campaignMongooseSchema = new Schema<Campaign>(
   {
@@ -168,20 +169,18 @@ const campaignMongooseSchema = new Schema<Campaign>(
     infos: { type: infosMongooseSchema },
     lores: { type: loresMongooseSchema },
     createdAt: { type: String, required: true },
-    updatedAt: { type: String, required: true },
+    updatedAt: { type: String, required: true }
   },
   { versionKey: false }
-);
+)
 
 const connection = mongoose.connection.useDb('campaign', {
   noListener: true,
-  useCache: true,
-});
+  useCache: true
+})
 
 export default class CampaignsModel extends MongoModel<Campaign> {
-  constructor(
-    public model = connection.model('campaign', campaignMongooseSchema)
-  ) {
-    super(model);
+  constructor(public model = connection.model('campaign', campaignMongooseSchema)) {
+    super(model)
   }
 }
