35f68ff0ce9f9ff875ef14df648c9ed72ad1bb8f
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2024-11-29 14:30:06 -0300

    feat: Add new prop to user entity

--- src/types/DatabaseEntities.ts
+++ src/types/DatabaseEntities.ts
@@ -21,3 +21,28 @@ export type UserEntities = 'Users' | 'UserDetails';
 export type CampaignsEntities = 'Campaigns';
 
 export type CharactersDnd5eEntities = 'CharactersDnd5e';
+
+export type userStatus = 
+| 'done' 
+| 'wait_to_complete' 
+| 'wait_to_confirm' 
+| 'wait_to_delete' 
+| 'wait_to_verify'
+| 'wait-to-confirm'
+| 'wait-to-complete'
+| 'wait-to-verify'
+| 'wait-to-change-email'
+| 'wait-to-start-password-change'
+| 'wait-to-finish-password-change'
+| 'wait-to-delete-user'
+| 'wait-to-secret-question'
+| 'wait-to-second-auth'
+| 'wait-to-activate-secret-question'
+| 'wait-to-update-secret-question'
+| 'wait-to-activate-two-factor'
+| 'wait-to-start-reset-two-factor'
+| 'wait-to-finish-reset-two-factor'
+| 'wait-to-start-email-change'
+| 'wait-to-finish-email-change'
+| 'wait-to-reset-profile'
+| 'wait-to-finish-delete-user';
